59daebc9613b88c641a9161398e4a1b09ad35eba
Misael García <misagtor@gmail.com>
2026-04-03 16:19:35 -0300

    update

--- bot.php
+++ bot.php
@@ -7,18 +7,42 @@ $input = file_get_contents('php://input');
 $data = json_decode($input, true);
 
 $chat_id = $update['message']['chat']['id'];
-$message = $update['message']['text'] ;
+$message = $update['message']['text'];
 
-switch($message) {
+switch ($message) {
     case '/start':
         $response = 'Iniciando Bot, un momento...';
         sendMessage($chat_id, $response);
         break;
     case 'prueba':
-        
+        $response = 'Prueba exitosa';
+        sendMessage($chat_id, $response);
+        break;
+    case 'prueba':
+
         sendMessage($chat_id, $response);
         break;
     default:
         $response = 'error';
 }
+
+function sendMessage($chat_id, $response)
+{
+    $url = $GLOBALS['website'] . '/sendMessage?chat_id=' . $chat_id . '&parse_mode=HTML&text=' . urlencode($response);
+    file_get_contents($url);
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
